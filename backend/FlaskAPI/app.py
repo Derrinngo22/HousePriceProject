d585420a8acd453d3d3b606dabafb8b51a3dbb31
Derrinngo22 <derrinngo@gmail.com>
2020-10-24 17:43:53 -0500

    test deploy to heroku

--- backend/FlaskAPI/app.py
+++ backend/FlaskAPI/app.py
@@ -1,5 +1,6 @@
+#https://medium.com/fintechexplained/flask-host-your-python-machine-learning-model-on-web-b598151886d
 import flask
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import json
 import pickle
 import numpy as np
@@ -7,12 +8,16 @@ import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['GET'])
+@app.route('/')
+def show_predict_house_form():
+    return render_template('home.html') 
+
+@app.route('/results', methods=['POST'])
 def predict():
     form = request.form
     # load model
     model = get_model()
-    oq = float(request.form['OQ'])
+    oq = float(form['OQ'])
     grl = np.log(float(request.form['GRL']))
     gc = float(request.form['GC'])
     tbsf = np.log(float(request.form['TBSF']))
@@ -20,14 +25,14 @@ def predict():
     yb = float(request.form['YB'])
     # oq, grl, gc, tbsf, fb, yb = 5, 123456, 2, 123456, 0, 1979
     input = pd.DataFrame({'OverallQual': oq, 'GrLivArea': grl, 'GarageCars': gc,'TotalBsmtSF': tbsf, 'FullBath': fb , 'YearBuilt': yb}, index=[0])
-    prediction = model.predict(input)[0]
-    print(str(model.predict(input)[0])+ "asdfasdf")
-    response = json.dumps({'response': str(prediction)})
-    return response, 200
+    prediction = model.predict(input)
+    # response = json.dumps({'response': str(prediction)})
+    return render_template('result.html', gc=gc,   predicted_price=np.expm1(prediction))
+    
 
 def get_model():
     return pickle.load(open("models/XGBmodel", 'rb'))
 
 
 if __name__ == '__main__':
- application.run(debug=True)
+ app.run(debug=True)
